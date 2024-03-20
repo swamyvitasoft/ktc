ac20bd2f596ef8efe6fe6069a09a9760fd67ac55
swamyvitasoft <swamy.vitasoft@gmail.com>
2024-03-20 23:11:53 +0530

    login component changes

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -2,40 +2,31 @@ import { Component, OnInit } from '@angular/core';
 import { AdminserviceService } from '../adminservice.service';
 import { Router } from '@angular/router';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent implements OnInit{
-  adminLoginform!:FormGroup
-    constructor(private api:AdminserviceService,
-      private router:Router,
-      private fb:FormBuilder){}
-
+export class LoginComponent implements OnInit {
+  adminLoginform!: FormGroup
+  constructor(private api: AdminserviceService,
+    private router: Router,
+    private fb: FormBuilder) { }
   ngOnInit(): void {
     this.adminLoginform = this.fb.group({
       mobileno: ['', [Validators.required]],
       password: ['', [Validators.required]]
     })
   }
-   
-  
-  login(){
-      this.api.adminLogin(this.adminLoginform.value).subscribe((res:any)=>{
-        console.log(res,'login')
-        if (res) {
-          localStorage.setItem('user', JSON.stringify(res.user));
-          localStorage.setItem('utoken', (res.token))
-
-          this.router.navigate(['/dashboard'])
-          alert('login sucesfully')
-        } else {
-          alert("Invalid Credentials")
-        }
-      })
+  login() {
+    this.api.adminLogin(this.adminLoginform.value).subscribe((res: any) => {
+      if (res) {
+        localStorage.setItem('user', JSON.stringify(res.user))
+        localStorage.setItem('utoken', (res.token))
+        this.router.navigate(['/dashboard'])
+      } else {
+        alert("Invalid Credentials")
+      }
+    })
   }
 }
-
-
